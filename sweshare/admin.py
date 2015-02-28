e8ba7a184ddcd12ade43fe1923801afbfff472bb
Karishma Changlani <karishmachanglani@gmail.com>
2015-02-28 16:41:43 -0500

    Fixed Node Database and added 3 sample nodes editable in admin

--- sweshare/admin.py
+++ sweshare/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from sweshare.models import Node
 
 # Register your models here.
+admin.site.register(Node)
--- sweshare/models.py
+++ sweshare/models.py
@@ -2,11 +2,8 @@ from django.db import models
 
 # Create your models here.
 class Node(models.Model):
-    node_text = models.CharField(max_length=200)
+    node_name = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
-
-
-class Edge(models.Model):
-    node = models.ForeignKey(Node)
-    choice_text = models.CharField(max_length=200)
-    no_of_nodes = models.IntegerField(default=0)
+    node_text = models.CharField(max_length=3000)
+    def __str__(self):
+        return self.node_name
